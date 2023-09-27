899897b53a61fbac38a69480af708a613bef1105
liuxueyong <liuxueyong@agora.io>
2023-09-27 18:18:47 +0800

    feat: 额外处理 ul, ol, Admonition 组件的换行

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,43 @@
+const ulRegex = /<ul>[\s\S]*?<\/ul>/g;
+const olRegex = /<ol>[\s\S]*?<\/ol>/g;
+const admonitionRegex =  /<Admonition.*?>[\s\S]*?<\/Admonition>/g;
+const firstTagRegex = new RegExp(`^\n\n${getTabs(3)}`);
+const lastTagRegex = new RegExp(`\n\n${getTabs(3)}$`);
+
+const startTagToReplace = {
+  ul: `\n\n${getTabs(3)}<ul>`,
+  ol: `\n\n${getTabs(3)}<ol>`,
+  admonition: `\n\n${getTabs(3)}<Admonition`
+};
+
+const closeTagToReplace = {
+  ul: `</ul>\n\n${getTabs(3)}`,
+  ol: `</ol>\n\n${getTabs(3)}`,
+  admonition: `\n${getTabs(3)}</Admonition>\n\n${getTabs(3)}`
+};
+
+const transformUlTag = (cell: string) => {
+  return cell.replace(ulRegex, (match) => {
+    return match.replace(/<ul>/g, startTagToReplace.ul).replace(/<\/ul>/g, closeTagToReplace.ul);
+  });
+};
+
+const transformOlTag = (cell: string) => {
+  return cell.replace(olRegex, (match) => {
+    return match.replace(/<ol>/g, startTagToReplace.ol).replace(/<\/ol>/g, closeTagToReplace.ol);
+  });
+};
+
+const transformAdmonitionTag = (cell: string) => {
+  return cell.replace(admonitionRegex, (match) => {
+    return match.replace(/<Admonition.*?>/g, (tag) => `\n\n${getTabs(3)}${tag}\n${getTabs(4)}`).replace(/<\/Admonition>/g, closeTagToReplace.admonition);
+  });
+};
+
+function getTabs (count: number = 1) {
+  return '  '.repeat(count);
+};
+
 const getCells = (line: string) => {
   const cells = line.split("|").map((cell) => cell.trim());
 
@@ -13,12 +53,41 @@ const getCells = (line: string) => {
 
 const getHeaders = (lines: string[]) => {
   return getCells(lines[0])
-    .map(
-      (cell) => ` {
-    label: '${cell}'
-  }`
-    )
+    .map((cell) => `${getTabs()}{\n${getTabs(2)}label: '${cell}'\n${getTabs()}}`)
     .join(",\n");
+};
+
+const cellTransformer = (cell: string) => {
+  let result = `\n${getTabs(2)}<Td>`;
+
+  if([ulRegex, olRegex, admonitionRegex].some((regex) => regex.test(cell))) {
+    result += `\n${getTabs(3)}`;
+
+    let _cell = cell;
+    _cell = transformUlTag(_cell);
+    _cell = transformOlTag(_cell);
+    _cell = transformAdmonitionTag(_cell);
+
+    // 当以 ul, ol, Admonition 开头时，处理多余换行
+    if(Object.values(startTagToReplace).some(tag => _cell.startsWith(tag))) {
+      _cell = _cell.replace(firstTagRegex, "");
+    }
+
+    result += _cell;
+
+    // 当以 ul, ol, Admonition 结尾时，处理多余换行
+    if(Object.values(closeTagToReplace).some(closeTag => result.endsWith(closeTag))) {
+      result = result.replace(lastTagRegex, `\n${getTabs(2)}`);
+    } else {
+      result += `\n${getTabs(2)}`;
+    }
+  } else {
+    result += cell;
+  }
+
+  result += '</Td>';
+
+  return result;
 };
 
 export const transformMdTable = (mdTableString: string) => {
@@ -34,11 +103,9 @@ ${getHeaders(lines)}
 
   for (const line of lines.slice(2)) {
     const cells = getCells(line);
-    result += `\n <Tr>`;
-    for (const cell of cells) {
-      result += `\n   <Td>${cell}</Td>`;
-    }
-    result += `\n </Tr>`;
+    result += `\n${getTabs()}<Tr>`;
+    result += cells.reduce((res, cell) => `${res}${cellTransformer(cell)}`, "");
+    result += `\n${getTabs()}</Tr>`;
   }
 
   result += `\n</Table>\n`;
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,6 +5,22 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
+const testMd = `| 参数               | 描述                                                         |
+| :----------------- | :----------------------------------------------------------- |
+| app_id            | <ul><li>111</li><li>222</li></ul>                   |
+| app_certificate   | <ul><li>111</li><li>222</li></ul>你的项目的 App 证书。                                        |
+| channel_name      | 频道名称，长度在 64 个字节以内。<ul><li>111</li><li>222</li></ul>以下为支持的字符集范围 |
+| uid              | 待鉴权用户的用户 ID 32 位无符号整数，范围为1到 (2³² - 1)， 并保证唯一性。 <ul><li>111</li><li>222</li></ul> |
+| app_id            | <ol><li>111</li><li>222</li></ol>                   |
+| app_certificate   | <ol><li>111</li><li>222</li></ol>你的项目的 App 证书。                                        |
+| channel_name      | 频道名称，长度在 64 个字节以内。<ol><li>111</li><li>222</li></ol>以下为支持的字符集范围 |
+| uid              | 待鉴权用户的用户 ID 32 位无符号整数，范围为1到 (2³² - 1)， 并保证唯一性。 <ol><li>111</li><li>222</li></ol> |
+| role             | <Admonition type="info" title="信息">Token 过期时，SDK 会触发 Token 过期回调。</Admonition>则权限会在生成后 10 分钟过期 |
+| token_expire     | AccessToken2 从生成到过期的时间长度。<Admonition type="info" title="信息">Token 过期时，SDK 会触发 Token 过期回调。</Admonition>单位为秒。 |
+| privilege_expire | 从 AccessToken2 生成到所有权限过期的时间长度，单位为秒。<Admonition type="info" title="信息">Token 过期时，SDK 会触发 Token 过期回调。</Admonition> |
+| privilege_expire | <Admonition type="info" title="信息">Token 过期时，SDK 会触发 Token 过期回调。<ul><li>111</li><li>222</li></ul> </Admonition> |
+| privilege_expire | <ul><li>111</li><li><Admonition type="info" title="信息">Token 过期时，SDK 会触发 Token 过期回调。</Admonition></li></ul> |`;
+
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
