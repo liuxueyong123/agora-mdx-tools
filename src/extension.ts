360f8195b127a2e778ec47cff219c9b7e98dfd9b
liuxueyong <liuxueyong@agora.io>
2024-01-16 17:13:58 +0800

    feat: 支持标题替换功能

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { transformMdTable } from './core';
+import { replaceFileOrFolder } from './replaceHeading';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('md-table-converter.covert2TableComponent', () => {
+	const disposable = vscode.commands.registerCommand('md-table-converter.covert2TableComponent', () => {
 		const editor = vscode.window.activeTextEditor;
 		if(!editor) {return;}
 		const allSelections = editor.selections;
@@ -29,7 +30,13 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 	});
 
+	const disposable2 = vscode.commands.registerCommand('md-table-converter.replaceHeading', (resource) => {
+		replaceFileOrFolder(resource.fsPath);
+		vscode.window.showInformationMessage('标题替换成功！');
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
